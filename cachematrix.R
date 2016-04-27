8b22a1b91fc34d826edf7d8ec670880c78e50c9a
Jaco Erasmus <jierasmus27@gmail.com>
2016-04-28 00:19:57 +0200

    Two functions added

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  cache <- NULL
 
+  set <- function(y) {
+          x <<- y
+          m <<- NULL
+  }
+  get <- function() x
+
+  setimatrix <- function(matrix) cache <<- matrix
+  getimatrix <- function() cache
+
+  list(set = set, get = get, setimatrix = setimatrix, getimatrix = getimatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cache <- x$getimatrix()
+
+  if (!is.null(cache)) {
+    print("Getting chached data")
+    return(cache)
+  }
+
+  matrix <- x$get()
+  cache <- solve(matrix, ...)
+  x$setimatrix
+  cache
 }
+
+cacheSolve()
